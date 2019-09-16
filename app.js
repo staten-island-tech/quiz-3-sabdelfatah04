f7f3fc40c1bfbaa2c4a86551b9dee368f874512b
sabdelfatah04 <50589044+sabdelfatah04@users.noreply.github.com>
2019-09-16 11:49:41 -0400

    stupid

---
+++ app.js
@@ -0,0 +1,16 @@
+//let bill = calcuateTip(bill);
+///let fullBill = ()
+function tipPaid(bill) {
+    let percentage;
+    if (bill < 50) {
+    percentage = .2;
+    } else if ( bill >= 50 && bill < 200) {
+    percentage = .15;
+    } else (tip >= 200); {
+    percentage = .1;
+    } return percentage * bill;
+}
+const bill = [140, 45, 270];
+const tips = [tipPaid(bill[0])]
+
+
