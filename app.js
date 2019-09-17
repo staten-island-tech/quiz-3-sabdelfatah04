c2812bf70505e2d0fc5fca4198c51dbecae22c5a
Sarah Abdelfatah <sarah.abdelfatah@sitechhs.com>
2019-09-16 22:34:37 -0400

    stupid#2

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-//let bill = calcuateTip(bill);
-///let fullBill = ()
 function tipPaid(bill) {
     let percentage;
-    if (bill < 50) {
+    if (bill < 50){
     percentage = .2;
-    } else if ( bill >= 50 && bill < 200) {
+    } else if ( bill >= 50 && bill < 200){
     percentage = .15;
-    } else (tip >= 200); {
+    } else {
     percentage = .1;
     } return percentage * bill;
 }
 const bill = [140, 45, 270];
-const tips = [tipPaid(bill[0])]
+const tips = [tipPaid(bill[0]), tipPaid(bill[1]) , tipPaid(bill[2]) ]
+console.log(tips);
+console.log([bill[0] + tips [0] , bill[1] + tips [1] , bill[2] + tips [2]]);
 
-
+let final = [bill[0] + tips [0] , bill[1] + tips [1] , bill[2] + tips [2]];
+// or let final = [161, 54, 297];
+const copy = final.filter(final => final > 100);
+console.log(copy);
