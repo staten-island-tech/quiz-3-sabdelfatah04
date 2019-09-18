b795b82e5733d5008dade60b8fbf309b23116f5a
sabdelfatah04 <50589044+sabdelfatah04@users.noreply.github.com>
2019-09-18 11:45:34 -0400

    ff

--- app.js
+++ app.js
@@ -17,3 +17,28 @@ console.log([final]);
 
 const copy = final.filter(final => final > 100);
 console.log(copy);
+
+const robInfo = {
+    fullName: 'Rob Grande',
+    mass: 78,
+    height: 1.69,
+    getBMI: function (){
+        BMI = (this.mass / ((this.height) * (this.height)))
+        return BMI;
+    }
+};
+const dejonInfo = {
+    fullName: 'Dejon Kurti',
+    mass: 110,
+    height: 1.95,
+    getBMI: function (){
+        BMI = (this.mass / ((this.height) * (this.height)))
+        return BMI;
+    }
+};
+console.log(dejonInfo.getBMI());
+console.log(robInfo.getBMI());
+
+if (dejonInfo.getBMI > robInfo.getBMI){
+    console.log(dejonInfo.fullName + 'has a higher BMI of ' + robInfo.getBMI)
+}
